2ffb4a548e91e334cbde9e027bd8e4f2f7eafcc7
Adriana <adis_amr@hotmail.com>
2020-02-17 00:53:15 -0600

    Login

---
+++ formvalidation/lib/src/pages/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:formvalidation/src/bloc/provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body:  Container(),
+      floatingActionButton: _crearBoton(context),
+    );
+  }
+
+  _crearBoton(BuildContext context){
+    return FloatingActionButton(
+      backgroundColor: Colors.deepPurple,
+      child: Icon(Icons.add),
+      onPressed: () => Navigator.pushNamed(context, 'producto'),
+    );
+  }
+}
