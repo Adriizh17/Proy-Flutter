f2ff8f9306ea97387c778688a65e09af059acdbe
Adriana <adis_amr@hotmail.com>
2020-04-24 19:45:37 -0500

    nuevos

---
+++ formvalidationBloc/lib/src/bloc/login_bloc.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:formvalidation/src/bloc/validators.dart';
+import 'package:rxdart/rxdart.dart';
+
+class LoginBloc with Validators {
+
+  /*  RXDart no trabaja con StreamController
+  final _emaiController     = StreamController<String>.broadcast();
+  final _passwordController = StreamController<String>.broadcast();*/
+
+  final _emaiController     = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
+
+  //Recuperar los datos del Stream
+  Stream<String> get emailStream    => _emaiController.stream.transform(validarEmail);
+  Stream<String> get passwordStream => _passwordController.stream.transform(validarPassword);
+
+  Stream<bool> get formValidStream => 
+    CombineLatestStream.combine2(emailStream, passwordStream, (e, p) => true);
+
+  //Insertar valores al Stream
+  Function(String) get changeEmail    => _emaiController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  //Obtener el ultimo valor ingresado a los Streams
+  String get email    => _emaiController.value;
+  String get password => _passwordController.value;
+
+  //Cerrar
+  dispose(){
+    _emaiController?.close();
+    _passwordController?.close();
+  }
+
+}
---
+++ formvalidationBloc/lib/src/bloc/provider.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:formvalidation/src/bloc/login_bloc.dart';
+export 'package:formvalidation/src/bloc/login_bloc.dart';
+
+import 'package:formvalidation/src/bloc/productos_bloc.dart';
+export 'package:formvalidation/src/bloc/productos_bloc.dart';
+
+class Provider extends InheritedWidget{
+
+  static Provider _instancia;
+
+  final loginBloc = LoginBloc();
+  final _productosBloc = new ProductosBloc();
+
+  //Determina si necesito regresar una nueva instancia de la clase o utilizar la existente
+  factory Provider({Key key, Widget child}){
+    if(_instancia == null){
+      _instancia = new Provider._internal(key: key, child: child);
+    }
+    return _instancia;
+  }
+
+  Provider._internal({Key key, Widget child})
+    : super(key: key, child: child);
+
+  /*Provider({Key key, Widget child})
+    : super(key: key, child: child);*/
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) => true;
+
+  static LoginBloc of(BuildContext context){
+    //return(context.inheritFromWidgetOfExactType(Provider) as Provider).loginBloc; 
+    return context.dependOnInheritedWidgetOfExactType<Provider>().loginBloc;
+  } 
+
+  static ProductosBloc productosBloc(BuildContext context){
+    return context.dependOnInheritedWidgetOfExactType<Provider>()._productosBloc;
+  } 
+}
